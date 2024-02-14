9bc997d6fa26e95a327b2819036f95d1a4bbb984
CorneNapier <CorneNapier.bootcamp@justit.co.uk>
2024-02-14 10:34:52 +0000

    Update day3.sql
    Added examples of on delete methods, and how rollback works

--- mySQL2/Day3/Day3.sql
+++ mySQL2/Day3/Day3.sql
@@ -16,3 +16,58 @@ select year(current_date());
 select now();
 -- Write a query that retruns employee FNAME in upper case --
 select ucase(fname) as 'First Name' from employee;
+-- -------------------------------------------------------------------------
+
+-- on delete cascade
+/* specify whether you want rows deleted in a child table when 
+corresponding rows are deleted in the parent table.*/
+
+-- on delete set null
+/* if a record in the parent table (referenced table) is deleted 
+then the corresponding entry or values in the child table (referencing table) will be set to NULL.*/
+
+create database world;
+use world;
+create table country
+(
+ id int primary key,
+ name varchar(20) not null
+);
+
+create table city
+(
+  cityId int primary key,
+  cityName varchar(50) not null,
+  countryId int
+);
+alter table city add foreign key(countryId) references country(id) on delete set null;
+
+insert into country values 
+(1, 'UK'),
+(2, 'USA'),
+(3, 'Germany');
+
+insert into city values (1, 'London', 1), (2, 'Manchester', 1), (3, 'New York', 2), (4, 'Berlin', 3);
+
+delete from country where name='uk';
+select* from city;
+select * from country;
+
+use company2;
+
+select * from department;
+select * from employee;
+
+delete from department where dname='Sales';
+
+-- Rollback is used when you have accidently deleted data
+rollback;
+-- requires set autocommit=0;
+set AUTOCOMMIT=0;
+commit;
+-- You can also Restore from backup
+-- You can not rollback the data deleted with truncate
+
+
+
+
