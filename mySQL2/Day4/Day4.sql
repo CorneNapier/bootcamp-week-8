4d8fb61f69ab57da664e1b14256a6de43a9bfb78
CorneNapier <CorneNapier.bootcamp@justit.co.uk>
2024-02-15 10:17:15 +0000

    Update day4.sql
    Added completed tasks relating to creating a stored procedure

--- mySQL2/Day4/Day4.sql
+++ mySQL2/Day4/Day4.sql
@@ -11,11 +11,12 @@ Stored procedures can be called and executed from application code or directly f
 
 /* change delemiter
 delimiter is the end of code statment think (;)
+unless command line is closed you will need to change the delimiter back to (;), whereas in workbench it will revert back by itself
 create procedure procedureName (optional parameter)
  begin
    
    
-   end newDelemitter
+   end newDelemiter
 */
 
 Delimiter ££
@@ -26,7 +27,7 @@ create procedure allEmployee()
    end ££   
    call allEmployee();
    
-   -- will have a parameter for first name
+   -- will have a parameter for first name (IN, OUT, INOUT)
    delimiter $$
    create procedure employee(IN firstName Varchar(20))
     begin
@@ -34,3 +35,29 @@ create procedure allEmployee()
 	end $$
    
 call employee('James');
+
+-- create a procedure which will delete an employee whos name you are passing as arg
+    -- show the table after changes
+use uniondb;
+delimiter $$
+create procedure deleteEmployee (IN firstname varchar(20))
+ begin
+    delete from employee where name=firstname;
+    select * from employee;
+ end $$
+ 
+ call deleteEmployee('Adam');
+
+-- create a procedure which will display employee record whose first name and last name you are passing as args
+use company1;
+delimiter $$
+create procedure displayFullName (IN firstName varchar(20), IN lastName varchar(20))
+  begin
+    select * from employee where fname=firstName and lname=lastName;
+  end $$
+  
+  select * from employee;
+call displayFullName('Ahmad', 'Jabbar');
+
+
+
